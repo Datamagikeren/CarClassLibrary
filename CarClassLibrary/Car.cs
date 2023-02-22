a82700f22b0bb602e498e6494649491d35786736
Datamagikeren <lasse4040@gmail.com>
2023-02-22 13:47:26 +0100

    Add fixes

---
+++ CarClassLibrary/Car.cs
@@ -0,0 +1,33 @@
+﻿namespace CarClassLibrary
+{
+    public class Car
+    {
+        public int ID { get; set; }
+        public string? Model { get; set; }
+        public int Price { get; set; }
+        public string? LicensePlate { get; set; }
+
+        public void ValidateModel()
+        {
+            if (Model?.Length < 4)
+            {
+                throw new ArgumentOutOfRangeException("Modelnavn må ikke være under 4 tegn (desværre Kia)");
+            }
+        }
+        public void ValidatePrice()
+        {
+            if (Price < 0)
+            {
+                throw new ArgumentOutOfRangeException("Pris skal være mere end 0");
+            }
+        }
+        public void ValidateLicensePlate()
+        {
+            if (LicensePlate.Length < 2 || LicensePlate.Length > 7) 
+            {
+                throw new ArgumentOutOfRangeException("Nummerpladen skal være på mere end 2 tegn, og må ikke være mere end 7");
+            }
+        }
+
+    }
+}
---
+++ CarClassLibraryTests/CarTests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CarClassLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarClassLibrary.Tests
+{
+    [TestClass()]
+    public class CarTests
+    {
+        Car car = new Car() { Model = "Mercedes", ID = 1, LicensePlate = "BA55368", Price = 10000 };
+        Car carModelUnder4 = new Car() { Model = "Kia", ID = 1, LicensePlate = "BA55368", Price = 10000};
+        Car carPriceUnder0 = new Car() { Model = "Mercedes", ID = 3, LicensePlate = "OK34934", Price = -100 };
+        Car carLicensePlateOver = new Car() { Model = "Mercedes", ID = 3, LicensePlate = "OB334350", Price = 10000 };
+        Car carLicensePlateUnder = new Car() { Model = "Mercedes", ID = 3, LicensePlate = "0", Price = 10000 };
+
+        [TestMethod()]
+        public void ValidateModelTest()
+        {
+            car.ValidateModel();
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => carModelUnder4.ValidateModel());
+        }
+        [TestMethod()]
+        public void ValidatePriceTest()
+        {
+            car.ValidatePrice();
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => carPriceUnder0.ValidatePrice());
+
+        }
+        [TestMethod()]
+        public void ValidateLicensePlateTest()
+        {
+            car.ValidateLicensePlate();
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => carLicensePlateOver.ValidateLicensePlate());
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => carLicensePlateUnder.ValidateLicensePlate());
+
+        }
+    }
+}
